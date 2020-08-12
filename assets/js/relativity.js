288c5235a3d0f2376ac6cf194fb93131fe4446e9
Zamicol <zamicol@gmail.com>
2020-08-11 19:03:29 -0600

    update equations

--- assets/js/relativity.js
+++ assets/js/relativity.js
@@ -1,13 +1,13 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	Slider();
+	LorentzSlider();
 	document.querySelector("#transformButton").addEventListener("click", function() {
-		Transform();
+		LorentzTransform();
 	});
 });
 
-function Transform() {
+function LorentzTransform() {
 	var lr = document.forms['lor'];
 
 	var bet = lr.elements["beta"].value;
@@ -15,14 +15,19 @@ function Transform() {
 	//console.log("beta: " + bet);
 	var gam = 1 / Math.sqrt(1 - Math.pow(bet, 2));
 
-	var cdt = lr.elements["timeIn"].value;
+	var dt = lr.elements["timeIn"].value;
 	var dx = lr.elements["xIn"].value;
+	var dm = lr.elements["mIn"].value;
+	var dg = lr.elements["gIn"].value;
 
-	lr.elements["timeOut"].value = cdt / bet;
-	lr.elements["xOut"].value = dx / bet;
+	lr.elements["gamma"].value = gam;
+	lr.elements["timeOut"].value = dt * bet;
+	lr.elements["xOut"].value = dx * bet;
+	lr.elements["mOut"].value = dm * bet;
+	lr.elements["gOut"].value = dg * bet;
 }
 
-function Slider() {
+function LorentzSlider() {
 	var slider = document.getElementById("lor_beta");
 	var output = document.getElementById("lor_beta_value");
 	output.value = slider.value; // Display the default slider value
@@ -30,6 +35,6 @@ function Slider() {
 	// Update the current slider value (each time you drag the slider handle)
 	slider.oninput = function() {
 		output.value = this.value;
-		Transform();
+		LorentzTransform();
 	}
 }
