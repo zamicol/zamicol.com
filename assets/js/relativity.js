79207f457d7202347c1658d6ef4f147f2f51ca42
Zamicol <zamicol@gmail.com>
2020-08-26 04:49:42 -0600

    git gravity playgroung working

--- assets/js/relativity.js
+++ assets/js/relativity.js
@@ -1,7 +1,17 @@
 "use strict";
 
-const C = 299792.458;
-const G = .0000000000667408;
+const C = 299792458; // m/s
+const G = 6.67408 * Math.pow(10, -11); // m kg s
+const SolarMass = 1.989 * Math.pow(10, 30); // kg
+const SolarRadius = 695700000; // m
+const EarthMass = 5.972 * Math.pow(10, 24)// kg
+const EarthRadius = 6378000;// m
+
+// Solar mass 1.989 × 10^30 kg
+// Solar radius 695,700 km
+// Solar gravity 273.95 m/s^2
+// Earth mass 5.972 × 10^24 kg
+// Eath Radius 6,378 km
 
 document.addEventListener("DOMContentLoaded", function(event) {
 	LorentzSlider();
@@ -47,41 +57,80 @@ function LorentzTransform() {
 }
 
 
-
+// Solar mass 1.989 × 10^30 kg
+// Solar radius 695,700 m
+// Solar gravity 273.95 m/s^2
 function GravityTransform() {
 	var lr = document.forms['gravity'];
 
 
+	var units = document.getElementById("gravityUnits").value;
 	var time = lr.elements["tIn"].value;
 	var length = lr.elements["xIn"].value;
-	var radius = lr.elements["rIn"].value;
-	var mass = lr.elements["mIn"].value;
+	var inputRadius = lr.elements["rIn"].value;
+	var inputMass = lr.elements["mIn"].value;
 
-	console.log(time + length + radius + mass);
-	console.log((2 * G * mass));
-	console.log(radius * Math.pow(C, 2));
-	console.log(((2 * G * mass) / radius * Math.pow(C, 2)));
+	// console.log("G: " + G);
+	// console.log("SolarMass: " + SolarMass);
+	// console.log("Inputs: " + time + length + inputRadius + inputMass);
+	// console.log("Using Units: " + units);
 
-// Solar mass 1.989 × 10^30
-// Solar radius 695,700
+	if (units == "solar") {
+		inputMass = inputMass * SolarMass;
+		inputRadius = inputRadius * SolarRadius;
+	}
 
-	console.log(1 - ((2 * G * mass) / radius * Math.pow(C, 2)));
-	console.log(Math.sqrt(1 - ((2 * G * mass) / radius * Math.pow(C, 2))));
-
-	var dt = time * Math.sqrt(1 - ((2 * G * mass) / radius * Math.pow(C, 2)));
-	var dg = "";
-	var esc = "";
-
-	if (Math.sign(dt) == -1 || dt.toString() == "NaN") {
-		dt = "Black Hole";
-		dg = "Black Hole";
-		esc = "Black Hole";
+	if (units == "earth") {
+		inputMass = inputMass * EarthMass;
+		inputRadius = inputRadius * EarthRadius;
 	}
 
 
+	// Newton Gravity 
+	var grav = (G * inputMass) / Math.pow(inputRadius, 2);
+	//console.log("Grav: " + grav);
+
+	// Time dilation
+	var dt = time * Math.sqrt(1 - ((2 * G * inputMass) / (inputRadius * Math.pow(C, 2))));
+	//console.log("Time dilation: " + dt);
+
+		// Length Contraction
+		var dx = length * Math.sqrt(1 - ((2 * G * inputMass) / (inputRadius * Math.pow(C, 2))));
+		//console.log("Time dilation: " + dt);
+
+	// Escape
+	var esc = Math.sqrt((2 * G * inputMass) / inputRadius);
+	//console.log("Escape: " + esc);
+
+
+	if (Math.sign(dt) == -1 || dt.toString() == "NaN") {
+		dt = "Black Hole";
+		dx = "Black Hole";
+		grav = "Black Hole";
+		esc = "Black Hole";
+	}
+
+	// Schwarzschild Radius 
+	var swarz = (2 * G * inputMass) / Math.pow(C, 2);
+	//console.log("Swarz: " + swarz);
+
+	// Schwarzschild Mass
+	var swarzM = (inputRadius * Math.pow(C, 2))/(2 * G);
+	console.log("Swarz Mass: " + swarzM);
+
+
+
+
+
+
 	document.querySelector(".gravityFrame .tOut").textContent = dt.toString();
-	document.querySelector(".gravityFrame .gOut").textContent = dg.toString();
-	document.querySelector(".gravityFrame .escOut").textContent = dg.toString();
+	document.querySelector(".gravityFrame .xOut").textContent = dx.toString();
+	document.querySelector(".gravityFrame .gOut").textContent = grav.toString();
+	document.querySelector(".gravityFrame .escOut").textContent = esc.toString();
+	document.querySelector(".gravityFrame .sczOut").textContent = swarz.toString();
+	document.querySelector(".gravityFrame .sczMOut").textContent = swarzM.toString();
+
+	
 }
 
 
